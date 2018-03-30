7e0523d8b00343f47162a5bb6684f5a6571a60cc
liuweijw <liuweijw.github@foxmail.com>
2018-03-30 23:40:40 +0800

    网关权限拦截优化

--- fw-cloud-system/fw-cloud-system-gateway/src/main/java/com/github/liuweijw/system/gateway/service/PermissionServiceImpl.java
+++ fw-cloud-system/fw-cloud-system-gateway/src/main/java/com/github/liuweijw/system/gateway/service/PermissionServiceImpl.java
@@ -6,27 +6,36 @@ import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 import org.springframework.stereotype.Service;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.util.CollectionUtils;
 
 import com.github.liuweijw.core.beans.system.AuthPermission;
+import com.github.liuweijw.core.commons.jwt.JwtUtil;
 import com.github.liuweijw.core.utils.StringHelper;
 
+@Slf4j
 @Service("permissionService")
 public class PermissionServiceImpl implements PermissionService {
 
 	@Autowired
     private MenuPermissionService apiPermissionService;
+	@Autowired
+    private RedisConnectionFactory redisConnectionFactory;
 
     private AntPathMatcher antPathMatcher = new AntPathMatcher();
 
     @SuppressWarnings("unchecked")
 	@Override
-    public boolean hasPermission(HttpServletRequest request, Authentication authentication) {
+    public boolean hasPermission(HttpServletRequest request,Authentication authentication) {
         // options 跨域配置，现在处理是通过前端配置代理，不使用这种方式，存在风险
         /*if (HttpMethod.OPTIONS.name().equalsIgnoreCase(request.getMethod())) {
             return true;
@@ -38,6 +47,21 @@ public class PermissionServiceImpl implements PermissionService {
         if (null == principal) return hasPermission;
         if (CollectionUtils.isEmpty(grantedAuthorityList)) return hasPermission;
 
+        String token = JwtUtil.getToken(request);
+        if(null == token) {
+        	log.warn("==> gateway|permissionService 未获取到Header Authorization");
+        	return hasPermission;
+        }
+        
+        if(!"anonymousUser".equals(principal.toString())) {
+        	RedisTokenStore tokenStore = new RedisTokenStore(redisConnectionFactory);
+            OAuth2AccessToken accessToken = tokenStore.readAccessToken(token);
+            if(null == accessToken || accessToken.isExpired()){
+            	log.warn("==> gateway|permissionService token 过期或者不存在");
+                return hasPermission;
+            }
+        }
+        
         // 接口层面做了缓存处理，后续可以继续优化
         Set<AuthPermission> permissions = new HashSet<AuthPermission>();
         for (SimpleGrantedAuthority authority : grantedAuthorityList) {
