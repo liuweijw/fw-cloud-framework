335e7e98d393bbc5da0258082af20f222cd2c487
liuweijw <liuweijw.github@foxmail.com>
2018-08-15 14:20:04 +0800

    优化微信消息模板消息发送

--- fw-cloud-business/fw-cloud-business-wechat/src/main/java/com/github/liuweijw/business/wechat/controller/WxMessageController.java
+++ fw-cloud-business/fw-cloud-business-wechat/src/main/java/com/github/liuweijw/business/wechat/controller/WxMessageController.java
@@ -1,8 +1,5 @@
 package com.github.liuweijw.business.wechat.controller;
 
-import lombok.extern.slf4j.Slf4j;
-import me.chanjar.weixin.common.error.WxErrorException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +17,9 @@ import com.github.liuweijw.commons.pay.enums.WxTemplateEnum;
 import com.github.liuweijw.commons.utils.Crypt;
 import com.github.liuweijw.commons.utils.StringHelper;
 
+import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.common.error.WxErrorException;
+
 /**
  * 微信公众号消息通过openId发送
  * 
@@ -36,8 +36,9 @@ public class WxMessageController {
 	@Autowired
 	private WechatInfoService	wechatInfoService;
 
-	@RequestMapping(value = "/send/{wechatId}")
 	@ResponseBody
+	@SuppressWarnings("deprecation")
+	@RequestMapping(value = "/send/{wechatId}")
 	public HttpResult sendWeixinTemplateMessage(@PathVariable("wechatId") String wechatId,
 			@RequestParam("message") String message) {
 		if (StringHelper.isBlank(message)) return new HttpResult().failure("发送消息内容不能为空");
@@ -46,7 +47,8 @@ public class WxMessageController {
 		if (null == wechatInfo)
 			return new HttpResult().failure("公众号wechatId[" + wechatId + "]不存在");
 
-		log.info("公众号消息发送：|wechatId[{}]|reqKey[{}]|message[{}]", wechatId, wechatInfo.getReqKey(),
+		log.info(
+				"公众号消息发送：|wechatId[{}]|reqKey[{}]|message[{}]", wechatId, wechatInfo.getReqKey(),
 				message);
 		String decryptMessage = Crypt.getInstance().decrypt(message, wechatInfo.getReqKey());
 		if (StringHelper.isBlank(decryptMessage)) return new HttpResult().failure("发送消息内容签名不正确");
@@ -57,6 +59,33 @@ public class WxMessageController {
 		WxTemplateEnum wxTemplateEnum = WxTemplateEnum.of(msgBean.getTemplateId());
 		try {
 			boolean isOk = messageService.sendWeixinTemplateMessage(wxTemplateEnum, msgBean);
+			if (isOk) return new HttpResult().success("SUCCESS");
+		} catch (WxErrorException e) {
+			e.printStackTrace();
+			return new HttpResult().failure("发送消息失败[" + e.getMessage() + "]");
+		}
+
+		return new HttpResult().failure("消息发送失败！");
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/sendCurrency/{wechatId}")
+	public HttpResult sendWeixinCurrencyTemplateMessage(@PathVariable("wechatId") String wechatId,
+			@RequestParam("message") String message) {
+		if (StringHelper.isBlank(message)) return new HttpResult().failure("发送消息内容不能为空");
+
+		WechatInfo wechatInfo = wechatInfoService.findByWechatId(wechatId);
+		if (null == wechatInfo)
+			return new HttpResult().failure("公众号wechatId[" + wechatId + "]不存在");
+
+		log.info("公众号消息发送：|wechatId[{}]|reqKey[{}]|message[{}]", wechatId, wechatInfo.getReqKey(), message);
+		String decryptMessage = Crypt.getInstance().decrypt(message, wechatInfo.getReqKey());
+		if (StringHelper.isBlank(decryptMessage)) return new HttpResult().failure("发送消息内容签名不正确");
+
+		MsgBean msgBean = JSONObject.parseObject(decryptMessage, MsgBean.class);
+		if (null == msgBean) return new HttpResult().failure("发送消息内容转换失败");
+		try {
+			boolean isOk = messageService.sendWeixinCurrencyTemplateMessage(msgBean);
 			if (isOk) return new HttpResult().success("SUCCESS");
 		} catch (WxErrorException e) {
 			e.printStackTrace();
--- fw-cloud-business/fw-cloud-business-wechat/src/main/java/com/github/liuweijw/business/wechat/service/MessageServiceImpl.java
+++ fw-cloud-business/fw-cloud-business-wechat/src/main/java/com/github/liuweijw/business/wechat/service/MessageServiceImpl.java
@@ -3,13 +3,6 @@ package com.github.liuweijw.business.wechat.service;
 import java.util.ArrayList;
 import java.util.List;
 
-import lombok.extern.slf4j.Slf4j;
-import me.chanjar.weixin.common.error.WxErrorException;
-import me.chanjar.weixin.mp.api.WxMpService;
-import me.chanjar.weixin.mp.bean.template.WxMpTemplateData;
-import me.chanjar.weixin.mp.bean.template.WxMpTemplateMessage;
-import me.chanjar.weixin.mp.bean.template.WxMpTemplateMessage.WxMpTemplateMessageBuilder;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +10,20 @@ import com.github.liuweijw.commons.pay.beans.MsgBean;
 import com.github.liuweijw.commons.pay.enums.WxTemplateEnum;
 import com.github.liuweijw.commons.utils.StringHelper;
 
+import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.common.error.WxError;
+import me.chanjar.weixin.common.error.WxErrorException;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.template.WxMpTemplateData;
+import me.chanjar.weixin.mp.bean.template.WxMpTemplateMessage;
+import me.chanjar.weixin.mp.bean.template.WxMpTemplateMessage.WxMpTemplateMessageBuilder;
+
 @Slf4j
 @Component
 public class MessageServiceImpl implements MessageService {
 
 	@Autowired
-	private WxMpService	wxMpService;
+	private WxMpService wxMpService;
 
 	@Override
 	public boolean sendWeixinTemplateMessage(WxTemplateEnum wxTemplateEnum, MsgBean msgBean)
@@ -83,6 +84,52 @@ public class MessageServiceImpl implements MessageService {
 			data.add(new WxMpTemplateData("keyword4", msgBean.getKeyword4(), "#173177"));
 			data.add(new WxMpTemplateData("remark", msgBean.getRemark(), "#173177"));
 		}
+		templateMessage.data(data);
+
+		WxMpTemplateMessage toMessage = null;
+
+		List<String> toUsers = msgBean.getToUser();
+		if (null != toUsers && toUsers.size() > 0) {
+			for (String toUser : toUsers) {
+				templateMessage.toUser(toUser.trim());
+
+				toMessage = templateMessage.build();
+				log.info("|send template msg start|" + toMessage.toString());
+				String returnMsg = wxMpService.getTemplateMsgService().sendTemplateMsg(toMessage);
+				log.info("|send template msg end  |" + returnMsg);
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public boolean sendWeixinCurrencyTemplateMessage(MsgBean msgBean) throws WxErrorException {
+		if (null == msgBean)
+			throw new WxErrorException(WxError.builder().errorCode(500).errorMsg("消息内容不能为空").build());
+		if (StringHelper.isBlank(msgBean.getTemplateId()))
+			throw new WxErrorException(WxError.builder().errorCode(500).errorMsg("消息内容不能为空").build());
+
+		WxMpTemplateMessageBuilder templateMessage = WxMpTemplateMessage.builder();
+		templateMessage.templateId(msgBean.getTemplateId());
+		if (!StringHelper.isBlank(msgBean.getLinkUrl())) templateMessage.url(msgBean.getLinkUrl());
+
+		List<WxMpTemplateData> data = new ArrayList<WxMpTemplateData>();
+
+		if (StringHelper.isNotBlank(msgBean.getFirst()))
+			data.add(new WxMpTemplateData("first", msgBean.getFirst(), "#173177"));
+		if (StringHelper.isNotBlank(msgBean.getKeyword1()))
+			data.add(new WxMpTemplateData("keyword1", msgBean.getKeyword1(), "#173177"));
+		if (StringHelper.isNotBlank(msgBean.getKeyword2()))
+			data.add(new WxMpTemplateData("keyword2", msgBean.getKeyword2(), "#173177"));
+		if (StringHelper.isNotBlank(msgBean.getKeyword3()))
+			data.add(new WxMpTemplateData("keyword3", msgBean.getKeyword3(), "#173177"));
+		if (StringHelper.isNotBlank(msgBean.getKeyword4()))
+			data.add(new WxMpTemplateData("keyword4", msgBean.getKeyword4(), "#173177"));
+		if (StringHelper.isNotBlank(msgBean.getKeyword5()))
+			data.add(new WxMpTemplateData("keyword5", msgBean.getKeyword5(), "#173177"));
+		if (StringHelper.isNotBlank(msgBean.getRemark()))
+			data.add(new WxMpTemplateData("remark", msgBean.getRemark(), "#173177"));
+
 		templateMessage.data(data);
 
 		WxMpTemplateMessage toMessage = null;
